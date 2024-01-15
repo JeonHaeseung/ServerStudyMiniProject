f137fbabdad3c1906a084f68651a39cdcc618d16
JeonHaeseung <414catherine@gmail.com>
2024-01-15 23:49:16 +0900

    init

---
+++ ServerStudyMiniProject/src/test/java/ServerStudyMiniProject/ServerStudyMiniProject/ServerStudyMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ServerStudyMiniProject.ServerStudyMiniProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStudyMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
