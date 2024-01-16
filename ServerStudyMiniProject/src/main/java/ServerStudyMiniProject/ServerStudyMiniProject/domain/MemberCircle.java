ee221ee33314be6b44d993c531d0b5532b18c0c6
JeonHaeseung <414catherine@gmail.com>
2024-01-16 21:07:27 +0900

    [feat] 맴버 서클 엔티티 개발

---
+++ ServerStudyMiniProject/src/main/java/ServerStudyMiniProject/ServerStudyMiniProject/domain/MemberCircle.java
@@ -0,0 +1,23 @@
+package ServerStudyMiniProject.ServerStudyMiniProject.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class MemberCircle {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_circle_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "circle_id")
+    private Circle circle;
+}
