88eec27d81f1f20879830147d36eae5ee4828c31
JeonHaeseung <414catherine@gmail.com>
2024-01-23 23:32:16 +0900

    [fix] 주소 엔티티 builder 타입으로 생성

--- ServerStudyMiniProject/src/main/java/ServerStudyMiniProject/ServerStudyMiniProject/domain/Address.java
+++ ServerStudyMiniProject/src/main/java/ServerStudyMiniProject/ServerStudyMiniProject/domain/Address.java
@@ -3,13 +3,22 @@ package ServerStudyMiniProject.ServerStudyMiniProject.domain;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Embeddable
-@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     private String country;
 
     private String city;
 
     private String zipcode;
+
+    @Builder
+    Address(String country, String city, String zipcode){
+        this.country = country;
+        this.city = city;
+        this.zipcode = zipcode;
+    }
+
 }
