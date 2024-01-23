fa3ba5f9f5018330c2dd3bc0fa772d6c68d0b247
JeonHaeseung <414catherine@gmail.com>
2024-01-23 23:29:16 +0900

    [feat] SecurityConfig 설정 및 보안 체인 생성

---
+++ ServerStudyMiniProject/src/main/java/ServerStudyMiniProject/ServerStudyMiniProject/auth/AuthDetailService.java
@@ -0,0 +1,19 @@
+package ServerStudyMiniProject.ServerStudyMiniProject.auth;
+
+import ServerStudyMiniProject.ServerStudyMiniProject.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailService implements UserDetailsService {
+    /* 사용자 정보를 데이터베이스에서 로드 */
+    private final MemberRepository memberRepository;
+
+    @Override
+    public AuthDetails loadUserByUsername(String email) {
+        Member member = memberRepository.findByEmail(email);
+        return new AuthDetails(member);
+    }
+}
