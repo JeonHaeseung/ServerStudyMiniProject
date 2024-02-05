f1d0076c12cb860573544d77297e25511eb841f5
JeonHaeseung <414catherine@gmail.com>
2024-02-05 19:46:39 +0900

    [feat] Circle 엔티티 생성자 추가

--- ServerStudyMiniProject/src/main/java/ServerStudyMiniProject/ServerStudyMiniProject/domain/Circle.java
+++ ServerStudyMiniProject/src/main/java/ServerStudyMiniProject/ServerStudyMiniProject/domain/Circle.java
@@ -2,12 +2,14 @@ package ServerStudyMiniProject.ServerStudyMiniProject.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Circle {
     @Id
     @GeneratedValue
@@ -20,4 +22,13 @@ public class Circle {
 
     @OneToMany(mappedBy = "circle") //외래키가 없으므로, MemberCircle 클래스의 member 필드로 역참조
     private List<MemberCircle> memberCircles = new ArrayList<>(); // MemberCircle 쪽이 다수이므로, List로 필드 만들기
+
+    public Circle(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public void changeDescription(String description){
+        this.description = description;
+    }
 }
